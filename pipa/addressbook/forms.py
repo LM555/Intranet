644e46e22063205bcc956af8f0ef1f573cb9161e
Matej <plus64@gmail.com>
2011-07-12 20:18:49 +0200

    new members form

--- pipa/addressbook/forms.py
+++ pipa/addressbook/forms.py
@@ -17,3 +17,34 @@ class ProfileForm(ModelForm):
 		exclude = ('user',)
 		# don't touch, this works in 1.1
 		fields = tuple(['email', 'first_name', 'last_name'] + [f.name for f in model._meta.fields if f.__class__.__name__ != 'AutoField'])
+
+
+
+
+class NewMemberForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    surname = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+    phone = forms.CharField(max_length=100, label='Phone no.', required=True)
+    username = forms.CharField(max_length=100, required=True)
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput(), min_length=6, max_length=100, required=True)
+    password2 = forms.CharField(label='Repeate pass.',widget=forms.PasswordInput(), min_length=6, max_length=100, required=True)
+    dogbert_login = forms.BooleanField(required=False)
+    jon_login = forms.BooleanField(required=False)
+    intranet_login = forms.BooleanField(initial=True, required=False)
+    create_email = forms.BooleanField(required=False)
+    
+    def clean(self):
+		cleaned_data = self.cleaned_data
+		passw1 = cleaned_data.get("password1")
+		passw2 = cleaned_data.get("password2")
+		
+		if passw1 and passw2:
+			if not passw1 == passw2:
+				self._errors['password2'] = self.error_class(['Passwords were not equal.'])
+				del cleaned_data['password1']
+				del cleaned_data['password2']
+
+		return cleaned_data
+
+
--- pipa/addressbook/views.py
+++ pipa/addressbook/views.py
@@ -6,21 +6,30 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from pipa.addressbook.models import PipaProfile
-from pipa.addressbook.forms import ProfileForm
-
+from pipa.addressbook.forms import ProfileForm, NewMemberForm
 
 def alumni(request):
 	profiles = PipaProfile.objects.filter(show_profile=True).order_by('user__first_name')
 	return render_to_response("alumni/alumni.html", RequestContext(request, locals()))
 
+
 @login_required
-def addressbook(request):
+def addressbook(request): 
 	profile = request.user.get_profile()
-	profile_form = ProfileForm(instance=profile, initial={'email': request.user.email, 'first_name': request.user.first_name, 'last_name': request.user.last_name})
+
+	default_profile_form = ProfileForm(instance=profile, initial={'email': request.user.email, 'first_name': request.user.first_name, 'last_name': request.user.last_name})	
+	profile_form = None
+	newmemberform = None
+	newmemberform_show = False
+	
 	
 	if request.method == 'POST':
+		submit_name = ("usersubmit" if 'usersubmit' in request.POST else ("profilesubmit" if 'profilesubmit' in request.POST else None))
+		
+
 		profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
-		if profile_form.is_valid():
+		
+		if profile_form.is_valid() and submit_name=='profilesubmit':
 			# this should probably be better integrated with LDAP
 			request.user.email = profile_form.cleaned_data['email']
 			request.user.first_name = profile_form.cleaned_data['first_name']
@@ -28,10 +37,40 @@ def addressbook(request):
 			request.user.save()
 			profile_form.save()
 			return HttpResponseRedirect(request.path)
+		
+		
+		newmemberform = NewMemberForm(request.POST)
+		
+		if newmemberform.is_valid() and submit_name=='usersubmit':
+			# @todo: dejansko dodaj uporabnika v sistem
+
+			newmemberform_show = True
+			return render_to_response( # on successful add of new member
+                                    "org/addressbook.html",
+                         			{'newmember_username': newmemberform.cleaned_data['username'],
+									 'profile_form': default_profile_form,
+									 'newmemberform': NewMemberForm(),
+									 'newmemberform_show': newmemberform_show},
+                                    context_instance=RequestContext(request)
+                                      )
+	
+		if submit_name=='usersubmit':
+			profile_form = default_profile_form
+			newmemberform_show = True
+			
+		elif submit_name=='profilesubmit':
+			newmemberform = NewMemberForm()
+
+	else:
+		profile_form = default_profile_form
+		newmemberform = NewMemberForm()
+		
 	
 	context = {'profile_form': profile_form,
 		'object_list': PipaProfile.objects.all(),
 		'user_list': User.objects.all().order_by('username'),
+		'newmemberform': newmemberform,
+		'newmemberform_show': newmemberform_show
 		}
 	
 	return render_to_response("org/addressbook.html", RequestContext(request, context))
